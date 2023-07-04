e23804653b2099ef5eaccf614115af5f4d9df7b8
waitingworld <707535808@qq.com>
2023-07-05 02:53:56 +0800

    Initial commit

---
+++ ruoyi-ui/src/views/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app-container home">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      // 版本号
+      version: "3.6.2",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped lang="scss">
+.home {
+
+}
+</style>
+
---
+++ sql/oracel/创建用户.sql
@@ -0,0 +1,5 @@
+CREATE USER ruoyi_cloud IDENTIFIED BY 123456;
+GRANT CONNECT, RESOURCE,dba TO ruoyi_cloud;
+SELECT tablespace_name, file_name FROM dba_data_files;
+CREATE TABLESPACE ruoyi_cloud_tablespace DATAFILE 'C:\APP\TABLESPACE\ruoyi_cloud_tablespace.dbf' SIZE 100M;
+ALTER USER ruoyi_cloud DEFAULT TABLESPACE ruoyi_cloud_tablespace;
